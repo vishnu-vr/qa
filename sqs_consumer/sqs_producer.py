a79f00ed988bd8b778c80c7f104a1cad6e0c9a3d
Vishnu Ramesh <vishnuramesh19@gmail.com>
2021-05-02 16:46:10 +0530

    bug fix

--- sqs_consumer/sqs_producer.py
+++ sqs_consumer/sqs_producer.py
@@ -5,11 +5,13 @@ import json
 
 settings = None
 
-sqs = boto3.resource('sqs', region_name=settings['AWS_REGION_NAME'],
+# dlq = sqs.get_queue_by_name(QueueName=os.environ["SQS_DEAD_LETTER_QUEUE_NAME"])
+
+def make_queue():
+    sqs = boto3.resource('sqs', region_name=settings['AWS_REGION_NAME'],
                                        aws_access_key_id=settings['AWS_ACCESS_KEY'],
                                        aws_secret_access_key=settings['AWS_SECRET_KEY'])
-queue = sqs.get_queue_by_name(QueueName=settings['AWS_SQS_QUEUE_NAME'])
-# dlq = sqs.get_queue_by_name(QueueName=os.environ["SQS_DEAD_LETTER_QUEUE_NAME"])
+    return sqs.get_queue_by_name(QueueName=settings['AWS_SQS_QUEUE_NAME'])
 
 if __name__ == "__main__":
     with open('../settings.json') as f:
@@ -20,6 +22,7 @@ if __name__ == "__main__":
         json_object = json.dumps({"message":message,"random":str(uuid.uuid4()),"email":email}, indent = 2) 
         # MessageDeduplicationId = input("MessageDeduplicationId : ")
         messagegroupid = input("messageGroupid : ")
+        queue = make_queue()
         response = queue.send_message(
         MessageBody=json_object,
         # MessageDeduplicationId=messageid,
